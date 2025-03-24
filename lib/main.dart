fb696654b673e39d8ee956ae924f314a14db5d1c
Shawnm-MAD <shawnmathew14@outlook.com>
2025-03-24 13:23:57 -0400

    Added Flutter form builder dependency to establish import in main.dart for app implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_form_builder/flutter_form_builder.dart';
 void main() => runApp(const MyApp());
 class MyApp extends StatelessWidget {
 const MyApp({super.key});
